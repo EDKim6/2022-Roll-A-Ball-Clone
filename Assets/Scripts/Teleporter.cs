b50231671b9be4ca5c861b8a8b7b0fef5b1823b4
EDKim6 <ththnim@nate.com>
2022-08-15 20:25:48 +0900

    Initial commit

---
+++ Assets/Scripts/Teleporter.cs
@@ -0,0 +1,25 @@
+ using UnityEngine;
+
+public class Teleporter : MonoBehaviour
+{
+    [SerializeField]
+    private Transform arrivePoint;
+    private AudioSource audioSource;
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // other 오브젝트의 태그가 "Player"이면
+        if (other.CompareTag("Player"))
+        {
+            // 오디오 재생
+            audioSource.Play();
+            // other(플레이어)의 위치를 arrivePoint로 설정
+            other.transform.position = arrivePoint.position;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Roll-A-Ball-Clone-2022
+ Roll A Ball Clone Study
